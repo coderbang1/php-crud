bd9125d35e6d22195bc44e6a582d79a5953954b6
coderbang1 <69970206+coderbang1@users.noreply.github.com>
2021-02-15 22:34:21 +0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php 
-$con=mysqli_connect('localhost','root','12345','mange');
+$con=mysqli_connect('');
 $id= $_GET['id'];
 $res=mysqli_query($con, "delete from employee where id='$id'");
 header("location:index.php");
